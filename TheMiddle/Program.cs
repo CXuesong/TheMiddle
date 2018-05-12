ce007b2fd09f9a8dc2b845dbda50e429d73b2659
forest93 <forest93@163.com>
2018-05-12 15:16:40 +0800

    添加项目文件。

---
+++ TheMiddle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheMiddle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
